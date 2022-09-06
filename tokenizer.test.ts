90deb4796529313db2afa2f97f4ff2cc066f090a
Matthew Greenberg <mgreenbe@ucalgary.ca>
2022-09-06 09:22:23 -0400

    remove useless code

--- tokenizer.test.ts
+++ tokenizer.test.ts
@@ -2,8 +2,6 @@ import { tokenGenerator } from "./tokenizer";
 import { expect, test } from "vitest";
 
 test("Throw if no tokenizer matches.", () => {
-    const num = { re: /^[0-9]+/, process: (s: string) => Number(s) };
-    const op = { re: /^[-+]/ };
     const source = "45 - abc";
     const tokenStream = tokenGenerator(source);
     expect(() => { return [...tokenStream]; }).toThrow();
